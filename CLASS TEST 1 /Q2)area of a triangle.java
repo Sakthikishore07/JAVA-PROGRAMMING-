e9e3b752d4a450e08ef838e299f0b70317ee0300
Sakthikishore07 <129191302+Sakthikishore07@users.noreply.github.com>
2024-02-15 12:52:53 +0530

    Create Q2)area of a triangle.java

---
+++ CLASS TEST 1 /Q2)area of a triangle.java
@@ -0,0 +1,8 @@
+public class Circle {
+    public static void main(String[] args) {
+        double b = 5;
+        double h = 7;
+        double area = 0.5* b * h;
+        System.out.print("the area of triangle  is : " +area);
+    }
+}
