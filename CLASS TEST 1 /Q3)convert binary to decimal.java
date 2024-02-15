a4aa57c61773e5ef2da92153c27ecb41f7ff178b
Sakthikishore07 <129191302+Sakthikishore07@users.noreply.github.com>
2024-02-15 12:53:52 +0530

    Create Q3)convert binary to decimal.java

---
+++ CLASS TEST 1 /Q3)convert binary to decimal.java
@@ -0,0 +1,7 @@
+public class BinaryToDecimal {
+    public static void main(String[] args) {
+        String binaryString = "1011"; // Example binary number
+        int decimal = Integer.parseInt(binaryString, 2);
+        System.out.println("Decimal equivalent: " + decimal);
+    }
+}
