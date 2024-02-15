564310b16879bcea3aecfb7b3c517e66cf69de52
Sakthikishore07 <129191302+Sakthikishore07@users.noreply.github.com>
2024-02-15 12:50:42 +0530

    Create Q1) area of circle .java

---
+++ CLASS TEST 1 /Q1) area of circle .java
@@ -0,0 +1,7 @@
+public class Circle {
+    public static void main(String[] args) {
+        double r= 5;
+        double area = 3.14 * r * r ;
+        System.out.print("the area of circle is : " +area);
+    }
+}
